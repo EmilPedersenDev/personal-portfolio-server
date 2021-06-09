0a40c1f40daf4dafc6416b94d35114156b26de16
Emil Pedersen <emil.pedersen@stretch.se>
2021-06-09 09:28:44 +0200

    adding cors header

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const app = express();
+
 const corsOptions = {
   origin: process.env.BASE_URL,
   optionsSuccessStatus: 200,
 };
 
-const app = express();
-app.use(express.json());
 app.use(cors(corsOptions));
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.BASE_URL);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({
